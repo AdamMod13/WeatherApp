b40ef67c1f4d4c9ca1b207d7888fa553faf1362d
Modlich Adam <adam.modlich13@gmail.com>
2023-04-11 16:52:59 +0200

    Dodaj pliki projektów.

---
+++ WeatherApp/CityWeather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    internal class CityWeather
+    {
+        public int cloud_pct { get; set; }
+        public int temperature { get; set; }
+        public int temp_feels_like { get; set; }
+        public int humidity { get; set; }
+        public int min_temp { get; set; }
+        public int max_temp { get; set;}
+        public double wind_speed { get; set; }
+
+        [JsonConverter] public CityWeather(string response)
+
+    }
+}
---
+++ WeatherApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city + "&country=" + country),
+                Headers =
+            {
+                { "X-RapidAPI-Key", "f1d830866bmsh894ef1aad2cc1a0p1a4176jsn801d806796dc" },
+                { "X-RapidAPI-Host", "weather-by-api-ninjas.p.rapidapi.com" },
+            },
+            };
+            using var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+    }
+}
